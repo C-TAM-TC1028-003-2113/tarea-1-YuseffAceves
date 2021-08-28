d16981f8fd1c4c28fb17a18e621cec636b4d458f
yuseff <A00832623@itesm.mx>
2021-08-28 18:44:46 -0500

    Tarea editorial

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -2,7 +2,7 @@ def main():
     # escribe tu código abajo de esta línea
     palabras = int(input("Dame el numero de palabras: "))
     pagina = palabras // 475
-    total = pagina + 1
+    total = (pagina) + 1
     costo = (total) * (60 * .9)
     print("El costo de la publicacion es:", costo)
 
